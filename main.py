b7075fd5e1e278fd4208b480d158e1ada8faa378
MyraChen <blue051280@gmail.com>
2019-08-04 12:55:45 +0800

    Add flask-migrate for handle db migrate

--- main.py
+++ main.py
@@ -1,28 +1,17 @@
 from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
-import datetime
+from flask_migrate import Migrate
 
 import config
-
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = config.DB_URI
 app.config.setdefault('SQLALCHEMY_TRACK_MODIFICATIONS', False)
 
 db = SQLAlchemy(app)
+import models
 
-class ShortUrl(db.Model):
-    __tablename__ = 'short_url'
-
-    key = db.Column('key', db.String(100), primary_key=True)
-    url = db.Column('url', db.Text())
-    created_at = db.Column('created_at', db.DateTime(), default=datetime.datetime.utcnow)
-    count = db.Column('count', db.BigInteger(), default=1)
-
-    def __init__(self, key, url, count):
-        self.key = key
-        self.url = url
-        self.count = count
+migrate = Migrate(app, db)
 
 @app.route('/')
 def index():
@@ -33,5 +22,4 @@ def convert():
     return request.form['origin_url']
 
 if __name__ == '__main__':
-    db.create_all()
     app.run(debug=True)
